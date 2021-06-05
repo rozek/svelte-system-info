7059e36e3a529e4f0f689aa4c195c457245b6ebf
Andreas Rozek <A.Rozek@gmx.de>
2021-06-05 05:25:44 +0200

    adds UMD and minification to build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
+// see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
+
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './svelte-system-info.ts',
   output: {
     dir: './',
-    format: 'umd',  // builds for both Node.js and Browser
-    name:'svelte-system-info', // required for UMD modules
+    format: 'umd', // builds for both Node.js and Browser
+    name:'System', // required for UMD modules
+    noConflict:true,
     sourcemap: true,
     exports: 'default',
   },
-  plugins: [typescript()],
+  plugins: [typescript(), terser()],
 };
