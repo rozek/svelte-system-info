7494e083f54249df1f326cda87d48004f5ef1c1b
Andreas Rozek <A.Rozek@gmx.de>
2021-06-15 10:33:34 +0200

    new build

---
+++ svelte-system-info.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    readonly BrowserName: string;
+    readonly BrowserVersion: any;
+    readonly OSName: string;
+    readonly OSVersion: string;
+};
+export default _default;
