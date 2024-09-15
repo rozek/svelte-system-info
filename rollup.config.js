626897ebf427a6c5381c4ffa79f9b50f22a34a68
Andreas Rozek <A.Rozek@gmx.de>
2024-09-15 05:47:18 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 // see https://github.com/rozek/build-configuration-study
 
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 
 export default {
   input: './src/svelte-system-info.ts',
   output: [
     {
-      file:     './dist/svelte-system-info.js',
+      file:     './dist/svelte-system-info.umd.js',
       format:    'umd', // builds for both Node.js and Browser
       name:      'System', // required for UMD modules
       noConflict:true,
